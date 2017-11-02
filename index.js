a8a1399c2f15f74c60cdf624c59b53cc9a5e9ec0
Jusitn Merz <jrmerz@gmail.com>
2017-11-01 17:49:59 -0700

    exposing element

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   AppStateInterface : require('./lib/AppStateInterface'),
   AppStateModel : require('./lib/AppStateModel'),
-  AppStateStore : require('./lib/AppStateStore')
+  AppStateStore : require('./lib/AppStateStore'),
+  'app-route' : require('./elements/app-route')
 }
