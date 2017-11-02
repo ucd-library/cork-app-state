747856946714de7cfee99993b1586a13b3eb055b
Jusitn Merz <jrmerz@gmail.com>
2017-11-01 17:47:33 -0700

    adding custom app-route to wires into app state

---
+++ elements/app-route.js
@@ -0,0 +1,62 @@
+import {Element as PolymerElement} from "@polymer/polymer/polymer-element"
+import "@polymer/app-route/app-location"
+
+import {Mixin, EventInterface} from "@ucd-lib/cork-app-utils"
+import queryString from "query-string"
+import AppStateInterface from "./lib/AppStateInterface"
+
+class AppRoute extends Mixin(PolymerElement)
+      .with(EventInterface, AppStateInterface) {
+  
+  static get template() {
+    return '<app-location url-space-regex="[[appRoutesRegex]]"></app-location>';
+  }
+
+  static get properties() {
+    return {
+      route: {
+        type: Object
+      },
+      appRoutes : {
+        type : Array,
+        value : []
+      },
+      appRoutesRegex : {
+        type : RegExp,
+        value : '_makeRegex(appRoutes)'
+      }
+    }
+  }
+
+  constructor() {
+    super();
+    window.addEventListener('location-changed', this._onLocationChange.bind(this));
+  }
+
+  ready() {
+    super.ready();
+    this._onLocationChange();
+  }
+
+  _makeRegex() {
+    let re = '^('+this.appRoutes
+                  .map(route => '/'+route)
+                  .push('/')
+                  .join('|') + ')';
+    this.appRoutesRegex = new RegExp(re, 'i');
+  }
+
+  _onLocationChange() {
+    this.location = {
+      pathname : window.location.pathname,
+      path : window.location.pathname.replace(/(^\/|\/$)/g, '').split('/'),
+      query : queryString.parse(window.location.search)
+    }
+
+    this._setAppState({
+      location : this.location
+    });
+  }
+}
+
+customElements.define('app-route', AppRoute);
